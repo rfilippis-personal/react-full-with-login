4f5ce35c26bc442e5463eb0add8111d549f273c4
RAFAELE L FILIPPIS <>
2022-10-24 12:53:49 -0300

    change login box position

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -28,7 +28,7 @@ export default function Login() {
   }, [user]);
 
   return (
-    <div className="shadow-xl mt-32 p-10 text-gray-700 rounded-lg">
+    <div className="shadow-xl p-10 text-gray-700 rounded-lg">
       <h2 className="text-2xl font-medium">Join Today</h2>
       <div className="py-4">
         <h3 className="py-4">Sign in with one of the providers</h3>
